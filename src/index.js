e769f807d41bcf8d0dd597d3e7d167d3c87fb73d
kmkzt <info.pscreator@gmail.com>
2019-03-28 18:25:40 +0900

    remove build file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-app.get('/', function (req, res) {
-    res.send('hello world');
-});
-app.listen(3000);
