8e5e9729a7d174d8285101e00537ca2bbc5f9a79
psc-kamei <info.pscreator@gmail.com>
2019-03-29 14:05:10 +0900

    replace pg

--- src/utils/connect/pg.ts
+++ src/utils/connect/pg.ts
@@ -1,20 +1,34 @@
-import * as pgConnect from 'pg-promise'
+import { Pool, QueryArrayConfig } from 'pg'
 
-const username = process.env.POSTGRES_USER || 'user'
-const password = process.env.POSTGRES_PASSWORD || 'pass'
-const database = process.env.POSTGRES_DB || 'database'
+const user = process.env.POSTGRES_USER || 'user'
 const host = '127.0.0.1'
-const port = '3306'
+const database = process.env.POSTGRES_DB || 'database'
+const password = process.env.POSTGRES_PASSWORD || 'pass'
+const port = 3306
 
-export const connectQuery = async () => {
-  try {
-    const connectPath = `postgres://${username}:${password}@${host}:${port}/${database}`
-    console.log('connect:' + connectPath)
-    const db = await pgConnect(/*options*/)(connectPath)
-    // const result = await db.query('SELECT $1:name FROM $2:name', ['*', 'users'])
-    const result = await db.query('SELECT * FROM users')
-    return result
-  } catch (err) {
-    console.log('ERROR:', err)
-  }
-}
+const connectPath = `postgres://${user}:${password}@${host}:${port}/${database}`
+
+const pool = new Pool({
+  connectionString: connectPath
+})
+export const query = (query: Partial<QueryArrayConfig>, value?: any[]) =>
+  pool.query(
+    {
+      rowMode: 'array',
+      ...query
+    } as QueryArrayConfig,
+    value
+  )
+
+// pg-promise
+// export const connectQuery = async () => {
+//   try {
+//     console.log('connect:' + connectPath)
+//     const db = pgConnect(/*options*/)(connectPath)
+//     // const result = await db.query('SELECT $1:name FROM $2:name', ['*', 'users'])
+//     const result = await db.query('SELECT * FROM users')
+//     return result
+//   } catch (err) {
+//     console.log('ERROR:', err)
+//   }
+// }
