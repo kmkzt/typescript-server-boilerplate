12a63602e7ea43cc1906bf4ef9f6dc7db54a370f
kmkzt <info.pscreator@gmail.com>
2019-04-01 22:27:06 +0900

    fix: patch envload

--- src/utils/env/index.ts
+++ src/utils/env/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from 'dotenv'
 import { resolve } from 'path'
 
 export const envload = () => {
+  // Todo: optimize env. Don't load env in heroku.
+  if (process.env.DATABASE_URL) return
   const envfile = `${process.env.NODE_ENV}.env`
   const envpath = resolve(__dirname, '../../../', envfile)
   console.log('load:' + envpath)
