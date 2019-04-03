16b840902827ef86e933647fa1d9f316c96f42a3
kmkzt <info.pscreator@gmail.com>
2019-04-03 13:59:33 +0900

    Add ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv')
+const isDevelop = process.env.NODE_ENV !== 'production'
+dotenv.config({ path: `${process.env.NODE_ENV}.env` })
+
+const user = process.env.POSTGRES_USER || 'user'
+const host = isDevelop ? 'localhost' : ''
+const database = process.env.POSTGRES_DB || 'database'
+const password = process.env.POSTGRES_PASSWORD || 'pass'
+const port = 5555
+
+module.exports = {
+  type: 'postgres',
+  host,
+  port,
+  username: user,
+  password,
+  database,
+  synchronize: isDevelop, // always database migrations
+  logging: false,
+  entities: ['src/entities/**/*.ts'],
+  migrations: ['db/typeorm/migrations/**/*.ts'],
+  subscribers: ['db/typeorm/subscribers/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'db/typeorm/migrations',
+    subscribersDir: 'db/typeorm/subscribers'
+  }
+}
