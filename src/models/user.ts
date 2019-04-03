7f82b68c29167bc85d5fbfc8fc1a35cdfc3ff730
kmkzt <info.pscreator@gmail.com>
2019-04-03 13:39:24 +0900

    Add User Entity

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,30 @@
 import Ajv from 'ajv'
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  OneToOne
+} from 'typeorm'
+
+@Entity()
+export class User extends BaseEntity {
+  // @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column()
+  username!: string
+
+  @Column()
+  color?: string
+
+  @Column()
+  profile?: string
+
+  @Column()
+  picture?: string
+}
 
 const schema = new Ajv({ removeAdditional: true }).compile({
   $async: true,
@@ -10,7 +36,13 @@ const schema = new Ajv({ removeAdditional: true }).compile({
     username: {
       type: 'string'
     },
-    email: {
+    color: {
+      type: 'string'
+    },
+    profile: {
+      type: 'string'
+    },
+    picture: {
       type: 'string'
     }
   }
